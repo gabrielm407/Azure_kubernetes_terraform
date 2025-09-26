54fca6d3eaf5d4ecc5e76eef1a7a0de2b14f5cb9
gabrielm407 <moffettgabriel@gmail.com>
2025-09-26 16:18:57 -0700

    breaking files up and providers changes

--- providers.tf
+++ providers.tf
@@ -4,22 +4,22 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 4.0"
     }
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = ">= 2.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.0"
-    }
+    # azuread = {
+    #   source  = "hashicorp/azuread"
+    #   version = ">= 2.0"
+    # }
+    # kubernetes = {
+    #   source  = "hashicorp/kubernetes"
+    #   version = ">= 2.0"
+    # }
+    # helm = {
+    #   source  = "hashicorp/helm"
+    #   version = ">= 2.0"
+    # }
+    # random = {
+    #   source  = "hashicorp/random"
+    #   version = ">= 3.0"
+    # }
   }
 }
 
@@ -28,16 +28,16 @@ provider "azurerm" {
   subscription_id = "60e43b59-17e9-40fa-bca0-48cf63303fd9"
 }
 
-provider "azurerm" {
-  alias           = "secondary"
-  features        {}
-  subscription_id = "YOUR_SECOND_SUBSCRIPTION_ID"
-  tenant_id       = "YOUR_SECOND_TENANT_ID" # optional, if needed
-}
+# provider "azurerm" {
+#   alias           = "secondary"
+#   features        {}
+#   subscription_id = "YOUR_SECOND_SUBSCRIPTION_ID"
+#   tenant_id       = "YOUR_SECOND_TENANT_ID" # optional, if needed
+# }
 
-module "example" {
-  source   = "./modules/example"
-  providers = {
-    azurerm = azurerm.secondary
-  }
-}
+# module "example" {
+#   source   = "./modules/example"
+#   providers = {
+#     azurerm = azurerm.secondary
+#   }
+# }
